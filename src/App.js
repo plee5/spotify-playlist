e5b3da26082625cebdc316527a06fa0d110d3a01
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2019-04-29 17:39:07 -0700

    new branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Playlist from "./Playlist.jsx";
-import TodoInput from './TodoInput.jsx';
 
 // function App() {
 
